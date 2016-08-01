a7a6c60fe1afddd79fadee475abd59e397251f0c
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2016-08-01 09:17:47 -0300

    corley

--- Corley/main.cpp
+++ Corley/main.cpp
@@ -37,7 +37,7 @@ int* vmin(int* vertices, Grafo &my_grafo){
 		if (retorno[i] == 1){ // se a aresta de id=i está contida no conjunto... 
 			Aresta *ai = arestas[i];
 			for (int j = i+1; j<arestas.size(); j++){
-				if (retorno[j] == 1){
+				if (retorno[j] == 1 && retorno[i] == 1){
 					if (a_domina_b(ai, arestas[j])){
 						retorno[j] = 0;
 					} else if (a_domina_b(arestas[j], ai)){
@@ -70,9 +70,9 @@ int main(){
 	}
 	int nA = id; // quantidade de arestas do grafo	
 
-	map<int, int* > *X = new map<int, int* >[n];
+	map<int, int* > *X = new map<int, int* >[n+1];
 	/* X é um vetor de map's. 
-	 * cada índice do vetor (0...n-1), representa o valor das iteracoes r do algoritmo do Corley
+	 * cada índice do vetor (0...n), representa o valor das iteracoes r do algoritmo do Corley
 	 * cada item o map é um valor de k = 1 ... mr
 	 * O tamanho de X nao aumenta. Somente aumenta o tamanho do map
      * dado um id do map com m elementos, busca é feita em O(mlogm)
@@ -80,7 +80,7 @@ int main(){
      incluidos na arvore. Se um indice i for 1, entao o vertice 1 está na arvore  
 	 */
 
-	 map<int, int* > *A = new map<int, int* >[n];
+	 map<int, int* > *A = new map<int, int* >[n+1];
 	 /* Semelhante ao vetor X, mas aqui o int* é um vetor de nA inteiros 
 	  * se um indice i do int* for 1, entao a i-esima aresta (0 à nA-1) está presente na arvore   	
 	  */
@@ -99,50 +99,53 @@ int main(){
 
     for (int r = 1; r<n; r++){
     	map<int, int* > Wr; 
+    	m[r+1] = 0;
     	/* pra guardar o retorno do Step2
 	     O indice do map é o valor de K (que será reinicializado a cada execucao do passo 2)
 	     o int* é o conjunto de id de arestas 
 	     */
+
     	for (int k = 1; k<=m[r]; k++){
     		Wr[k] = vmin(X[r][k], my_grafo);  //Step2
     	}
 	     
 	     for (int s = 1; s<=m[r]; s++){
+
 	     	int *wrs = Wr[s];
 	     	//int *wrs = new int[]
-	     	m[r+1] = 0;
 	     	map <int, Aresta *> arestas = my_grafo.get_allArestas();
 	 
 	     	for (int j = 0; j<nA; j++){
 	     		if (wrs[j]==1){ // para escolher uma aresta em wrs
 					wrs[j]=0;
 					m[r+1]+=1;
+					cout<<"s = "<<s<<"  r = "<<r<<" m[r+1] = "<<m[r+1]<<endl;
+		
 					X[r+1][m[r+1]] = new int[n];
 					for (int y = 0; y<n; y++){
 					 X[r+1][m[r+1]][y] = X[r][s][y]; // apenas para fazer uma copia // pode MELHORAR
 					}
-					//A[r+1][m[r+1]] = new int[nA];
-					//for (int y = 0; y<nA; y++){
-					 //A[r+1][m[r+1]][y] = A[r][s][y]; // apenas para fazer uma copia // pode MELHORAR
-				//	}
+					A[r+1][m[r+1]] = new int[nA];
+					for (int y = 0; y<nA; y++){
+					 A[r+1][m[r+1]][y] = A[r][s][y]; // apenas para fazer uma copia // pode MELHORAR
+					}
 					int p = arestas[j]->getOrigem();
 					int q = arestas[j]->getDestino();
 					X[r+1][m[r+1]][p] = 1; // redundante
 					X[r+1][m[r+1]][q] = 1;
-					//A[r+1][m[r+1]][j] = 1;
+					A[r+1][m[r+1]][j] = 1;
 					if (m[r+1] != 1){
-						cout<<"verificar duplicata!!!"<<endl;
-						cout<<"s = "<<s<<"  r = "<<r<<"m[r+1] = "<<m[r+1]<<endl;
+						//cout<<"verificar duplicata!!!"<<endl;
 					}
 	     		}
 	     	}
 	    }
     }
     cout<<"saiu"<<endl;
-    for (int k = 0; k < m[n]; k++){ // cada arvore formada
+    for (int k = 1; k <= m[n]; k++){ // cada arvore formada
     	int *arestas  = A[n][k]; 
     	map <int, Aresta *> arestasPtr = my_grafo.get_allArestas();
-    	cout<<"Arvore "<<k+1<<endl;
+    	cout<<"Arvore "<<k<<endl;
     	for (int a = 0; a<nA; a++){ // cada aresta da arvore
 			if (arestas[a] == 1){
     			cout<<arestasPtr[a]->getOrigem() << " ";
