9b57c0acc59d157f5a48c292442800b47e7b29d1
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2016-09-02 08:23:07 -0300

    testes

--- SteinerRadzik/scriptTeste.sh
+++ SteinerRadzik/scriptTeste.sh
@@ -2,14 +2,14 @@
 
 for i in 5 7 10 12 15 17 20 22 25 27 30 33 35 38 40 42 45 48 50
 do
-	./steinerRadzik < ../Instâncias/correlated/$i.1/$i.corr1.in > Testes/correlated/$i.corr1_12.out
+	./steinerRadzik < ../Instâncias/correlated/$i.1/$i.corr1.in > Testes/correlated/$i.corr1_15.out
 	echo "Done  $i";
 done 
 echo "DONE correlated"
 
 for i in 5 7 10 12 15 17 20 22 25 27 30 33 35 38 40 42 45 48 50
 do
-	./steinerRadzik < ../Instâncias/anticorrelated/$i.1/$i.anticorr1.in > Testes/anticorrelated/$i.anticorr2.out
+	./steinerRadzik < ../Instâncias/anticorrelated/$i.1/$i.anticorr1.in > Testes/anticorrelated/$i.anticorr3.out
 	echo "Done  $i";
 done 
 echo "DONE anticorrelated"
