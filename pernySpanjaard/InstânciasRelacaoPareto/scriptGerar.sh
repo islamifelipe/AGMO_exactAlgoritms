fe4e72ba1d0b371248525f06a0a87376e86f3d0c
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-01-09 08:19:29 -0300

    intancias

--- pernySpanjaard/InstânciasRelacaoPareto/scriptGerar.sh
+++ pernySpanjaard/InstânciasRelacaoPareto/scriptGerar.sh
@@ -2,37 +2,35 @@
 
 for i in 5 7 10 12 15 17 20 22 25 27 30 33 35 38 40 42 45 48 50
 do
-	.././dominanceRelation < ../../Instâncias/correlated/$i.1/$i.corr1.in > correlated/$i.corr1.in
-	echo "Done  $i";
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/correlated/$i.corr2.in > correlated/$i.corr2.in
+	echo "Done  $i.2";
+
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/correlated/$i.corr3.in > correlated/$i.corr3.in
+	echo "Done  $i.3";
 done 
 echo "DONE correlated"
 
 for i in 5 7 10 12 15 17 20 22 25 27 30 33 35 38 40 42 45 48 50
 do
-	.././dominanceRelation < ../../Instâncias/anticorrelated/$i.1/$i.anticorr1.in > anticorrelated/$i.anticorr1.in
-	echo "Done  $i";
+
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/anticorrelated/$i.anticorr2.in > anticorrelated/$i.anticorr2.in
+	echo "Done  $i.2";
+
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/anticorrelated/$i.anticorr3.in > anticorrelated/$i.anticorr3.in
+	echo "Done  $i.3";
+
 done 
 echo "DONE anticorrelated"
 
 
 for i in 5 7 10 12 15 17 20 22 25 27 30 33 35 38 40 42 45 48 50
 do
-	.././dominanceRelation < ../../Instâncias/concave/$i.1/$i.conc1.in > concave/$i.conc1.in
-	echo "Done  $i";
+
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/concave/$i.conc2.in > concave/$i.conc2.in
+	echo "Done  $i.2";
+
+	.././dominanceRelation < ../../AllInstâncias/2_OBJETIVOS/concave/$i.conc3.in > concave/$i.conc3.in
+	echo "Done  $i.3";
+
 done 
 echo "DONE concave"
-
-for i in 6 12 18 20 24 27 30 33 36 38 40 42 44 48 50 52 55 58 60 63 66 68 70 75 77 80 82 86 90 93 96 100
-do
-	.././dominanceRelation < ../../GridInstâncias/correlate/$i.gridcorr1.in > Grid/Correlated/$i.gridcorr1.in
-	echo "Done  $i";
-done 
-echo "DONE correlated"
-
-
-for i in 6 12 18 20 24 27 30 33 36 38 40 42 44 48 50 52 55 58 60 63 66 68 70 75 77 80 82 86 90 93 96 100
-do
-	.././dominanceRelation < ../../GridInstâncias/anticorrelate/$i.gridanticorr1.in > Grid/Anticorrelated/$i.gridanticorr1.in
-	echo "Done  $i";
-done 
-echo "DONE anticorrelated"
