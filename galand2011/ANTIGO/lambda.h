ca17040e397efb2011f54f7a4e267ad6ebaac159
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-01-19 09:29:01 -0300

    Resultados 3 e 4 objetivos Lokman e Koksalan

--- galand2011/ANTIGO/lambda.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LAMBDA_H_
-#define _LAMBDA_H_
-#include <utility>      // std::pair
-using namespace std;
-pair<float , float> algorithm1(float v[4]);
-float psi(float x1, float x2, float v[4]);
-float w(float x);
-
-#endif
