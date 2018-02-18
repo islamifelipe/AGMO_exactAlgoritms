164af0eed8097b350a14f1c01bda4d0ff9caa580
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2018-02-18 11:25:47 -0300

    SR corrigido quase là

--- SteinerRadzik_NOVO/2_OBJETIVOS/main.cpp
+++ SteinerRadzik_NOVO/2_OBJETIVOS/main.cpp
@@ -27,7 +27,7 @@
 using namespace std;
 
 
-#define MAX_K_BEST 1000000 // este valor representa somenta a k-ésima best árvore encontrada e NAO O TAMANHO DA HEAP
+#define MAX_K_BEST 3000000 // este valor representa somente a k-ésima best árvore encontrada e NAO O TAMANHO DA HEAP
 
 // ATENCAO: esta implementençao é para GRAFOS COMPLETOS
 
@@ -35,6 +35,7 @@ double custos[NUMOBJETIVOS][NUMEROVERTICES][NUMEROVERTICES];
 // OBJETIVOS NUMVERTICES NUMVERTICES
 
 vector<SolucaoEdgeSet *> suportadas;
+vector<SolucaoEdgeSet *> noSuportadas;
 
 struct twoint {
 	int a;
@@ -107,6 +108,19 @@ inline bool compareHeap (SolucaoEdgeSet *a, SolucaoEdgeSet *b){
 	return a->objetivoGeral > b->objetivoGeral; // > mesmo
 }
 
+bool equalfloat(float a, float b){
+	return abs(a-b)<=PRECISAO;
+	
+}
+
+bool maiorQuefloat(float a, float b){ //retorna true se a>b
+	return a-b>PRECISAO;
+}
+
+
+bool maiorIgualQuefloat(float a, float b){ //returna true se a>=b
+	return maiorQuefloat(a, b)||equalfloat(a, b);	
+}
 
 
 void borderSearch () {
@@ -120,12 +134,12 @@ void borderSearch () {
 		atual.b = fila.front().b;
 		fila.pop();
 		// cout << "T3" << endl;
-		for (int j = 0; j < (NUMEROVERTICES*(NUMEROVERTICES-1))/2; j++) { // GRAFOS COMPLETOS
+		for (int j = 0; j < NUMARESTAS; j++) { // GRAFOS COMPLETOS
 			arestas[j].fit = arestas[j].c1*(suportadas[atual.a]->getObj(1)-suportadas[atual.b]->getObj(1)) +
 					 arestas[j].c2*(suportadas[atual.b]->getObj(0)-suportadas[atual.a]->getObj(0));
 		}
 		// cout << "T4" << endl;
-		stable_sort (arestas, arestas + (NUMEROVERTICES*(NUMEROVERTICES-1))/2, auxEdgeStructCompFit);
+		stable_sort (arestas, arestas + NUMARESTAS, auxEdgeStructCompFit);
 		// cout << "T5" << endl;
 		SolucaoEdgeSet *pii = new SolucaoEdgeSet(NUMEROVERTICES-1);
 		lkr.executar (*pii, arestas); //esta versao do kruskal considera que as arestas ja estao ordenadas
@@ -192,6 +206,7 @@ void Partition(SolucaoEdgeSet *particao, float xl, float yl, float xll, float yl
 		int dest = particao->edges[i][1];
 		if (particao->status[orig][dest]==0){ // para cada aresta opcional
 			P1.status[orig][dest] = 2; // proibida
+			P1.status[dest][orig] = 2; //
 			SolucaoEdgeSet *nova = new SolucaoEdgeSet(NUMEROVERTICES-1);
 			*nova = P1; // copia também a partiçao (arestas proibidas e obrigatorias)
 			res = lkr.executar_particao(*nova, arestas);
@@ -205,42 +220,201 @@ void Partition(SolucaoEdgeSet *particao, float xl, float yl, float xll, float yl
 				delete nova;
 			}
 			P1.status[orig][dest] = 1;
+			P1.status[dest][orig] = 1;
 		}
 	}
 }
 
-SolucaoEdgeSet* AllSpaningTree(float xl, float yl, float xll, float yll){
+SolucaoEdgeSet* AllSpaningTree(float xl, float yl, float xll, float yll,float a, float bM){
 
 	SolucaoEdgeSet *particao = Heap.front();
 	std::pop_heap (Heap.begin(),Heap.end(),compareHeap); Heap.pop_back();
 
-	Partition(particao, xl, yl, xll, yll);
-
+	float x = particao->getObj(0);
+	float y = particao->getObj(1);
+	
+	if (maiorIgualQuefloat(y,(a*x+bM))==false){ // Caso o ponto minimo passa da linha maxima, nao precisa calcular novamente		
+		Partition(particao, xl, yl, xll, yll);
+	}
 	return particao;
+}
+
+
+
+// /* 
+// # retorna verdadeiro se o ponto kbest está dentro da regiao viável
+// # se verdeiro, atualiza a regiao viável
+// */	
+bool isInViableRegion(list< pair<float, float> > &regiaoViavel, float x, float y){
+	bool retorno = false; // por default, o ponto nao está na regiao viavel 
+	//This check is done by a simple linear search through the list of the consecutive corner points defining the viable region.
+	//cout<<"\nkbest = ("<<x<<"'"<<y<<")"<<endl;
+	for (list< pair<float, float> >::iterator it = regiaoViavel.begin(); it!=regiaoViavel.end(); it++){
+		pair<float, float> ponto = (*it); // um ponto extremo que delimita a regiao viável
+		float ponto_x = ponto.first, ponto_y = ponto.second;
+		/* o k_best_x deve ser menor que o ponto_x   E   o k_best_y deve ser menor que o ponto_y  
+		caso isso nao seja verdadeiro para nenhum ponto, entao o kbest nao está na regiao viavel*/ 
+
+		//cout<<"corner = ("<<ponto_x<<"'"<<ponto_y<<")"<<endl;
+		
+		if (x<ponto_x && y<ponto_y){//caso o ponto esteja na regiao viavel, atualizamos-a imediatamente
+			pair<float, float> ponto1 = make_pair(x, ponto_y);
+			pair<float, float> ponto2 = make_pair(ponto_x, y);
+			regiaoViavel.insert(it, ponto1);
+			regiaoViavel.insert(it, ponto2);
+			regiaoViavel.remove(ponto); // remove-se o ponto (corner), e adiciona-se os novos dois pontos formados
+			retorno = true;
+			break; // se o ponto está regiao viavel, nao há mais rezao para percorrer-la 
+		}
+	}
+	return retorno;
+}
+
+pair<float, float> getMaiorDistante(float a, float b, float c, list< pair<float, float> > regiaoViavel){
+	pair<float, float>  pontoR = (*regiaoViavel.begin());
+	float max = ((float)abs(a*pontoR.first + b*pontoR.second + c))/((float) sqrt(a*a + b*b));
+	for (list< pair<float, float> >::iterator it = regiaoViavel.begin(); it!=regiaoViavel.end(); it++){
+		pair<float, float> ponto = (*it);
+		float distancia = abs(a*ponto.first + b*ponto.second + c)/sqrt(a*a + b*b); // distância de ponto a r
+ 		if (maiorQuefloat(distancia,max)){
+ 			max = distancia;
+ 			pontoR = ponto;
+ 		}
+	}
+	return pontoR;
+}
+
+
+
+// /* 
+// Recebe o grafo e a lista de solucoes soportads (as extremas do eixo convexo) 
+// Retorna a lista de solucoes nao-suportadas (aquelas que estao dentro de um triângulo formado por duas soluceos extremas)
+// Assim, teremos separadas as solucoes suportadas e nao suportadas
+// */
+void phase2KB(){
+	vector<SolucaoEdgeSet *>::iterator it = suportadas.begin(); 
+	int contador = 0;
+	int size = suportadas.size();
+	while (contador<size-1){
+		SolucaoEdgeSet *pp = *(it);
+		SolucaoEdgeSet *pq = *(++it);
+		// int *p = pp.first; 
+		// int *q = pq.first;
+		float yp, yq, xp, xq;
+		list< pair<float, float> > regiaoViavel; // lista de pontos que delimitam a regiao viável.
+	
+		xp = pp->getObj(0); //pp.second.first;
+		yp = pp->getObj(1); //pp.second.second;
+		xq = pq->getObj(0); //pq.second.first;
+		yq = pq->getObj(1); //pq.second.second;
+
+		regiaoViavel.push_back( make_pair(xq, yp));// inicialmente, a regiao viável é composta por um unico ponto (o âgulo reto do triângulo cuja hipotenusa é a reta entre p-q -- ver algoritmo origial)
+		
+		// determina a reta p-q (hipotenusa)
+		float a, b; // pra determinar a equacao da reta p-q na forma ax+b = y
+		a = ((float)(yp-yq))/((float) (xp-xq)); // coeficiente angular da reta p-q
+		b = yq - a*xq; // coeficiente linear de p-q
+		pair<float, float> maisDistante = getMaiorDistante(a, -1, b, regiaoViavel);
+		//Agora determinamos a reta de custo maximo, ou seja, a reta paralela à p-q que passa pelo ponto mais distante
+		//cout<<"Mais distante : "<<maisDistante.first<<", "<<maisDistante.second<<endl;
+		float bM = maisDistante.second - a*maisDistante.first; // coeficiente angular da reta de custo maximo ax+bM = y
+
+		for (int j = 0; j < NUMARESTAS; j++) { // GRAFOS COMPLETOS
+			arestas[j].fit = arestas[j].c1*(yp-yq) + arestas[j].c2*(xq-xp);
+		}
+
+		stable_sort (arestas, arestas + NUMARESTAS, auxEdgeStructCompFit);
+
+		SolucaoEdgeSet *pii = new SolucaoEdgeSet(NUMEROVERTICES-1);
+		bool res = lkr.executar_particao (*pii, arestas); //esta versao do kruskal considera que as arestas ja estao ordenadas
+		if (res){
+			float x = pii->getObj(0);
+			float y = pii->getObj(1);
+			pii->objetivoGeral = x*(yp-yq)+y*(xq-xp);
+			Heap.push_back(pii);
+			make_heap(Heap.begin(),Heap.end(), compareHeap);
+		} else delete pii;
+
+		for (int k = 1; k<MAX_K_BEST && Heap.size()!=0; k++){
+			SolucaoEdgeSet *nova = AllSpaningTree(xp,yp, xq,yq,a, bM);
+			float x = nova->getObj(0);
+			float y = nova->getObj(1);
+			if (isInViableRegion(regiaoViavel, x, y)){ // se estiver na regiao viavel
+				SolucaoEdgeSet * nn = new SolucaoEdgeSet(NUMEROVERTICES-1);
+				*nn = *nova;
+				noSuportadas.push_back(nn);
+				//Agora atualizamos a reta de custo maximo, ou seja, a reta paralela à p-q que passa pelo ponto mais distante
+				bM = (float)maisDistante.second - (float)a*maisDistante.first; // coeficiente angular da reta de custo maximo ax+bM = y
+			
+			} else if ( maiorIgualQuefloat(y,(a*x+bM))) { //s on or past maximum cost line 
+			//	cout<<"K break = "<<k<<endl;
+				k = MAX_K_BEST+1; // sai;
+
+			}
+			delete nova;
+		}
+		//LIMPA A HEAP
+		for (std::vector<SolucaoEdgeSet* >::iterator ittt = Heap.begin() ; ittt != Heap.end(); ++ittt){
+			delete *ittt;
+		}
+		Heap.clear();
+		//LIMPA A HEAP
+		contador++;
+	}
 }
 
 int main(){
 
 	input(); // ler instância
-	// FirstPhase();
-	// stable_sort (suportadas.begin(), suportadas.end(),auxEdgeStructCompSolutions);
-	
-	stable_sort (arestas, arestas + NUMARESTAS,auxEdgeStructLexCompObj1);
-
-	SolucaoEdgeSet *pii = new SolucaoEdgeSet(NUMEROVERTICES-1);
-	lkr.executar (*pii, arestas); //esta versao do kruskal considera que as arestas ja estao ordenadas
-	
-	pii->objetivoGeral = pii->getObj(0);
-	Heap.push_back(pii);
-	
-	for (int i=0; i<100 && Heap.size()>0; i++){
-		float xl  = 0;
-		float yl  = 1;
-		float xll = 0;
-		float yll = 0;
-		SolucaoEdgeSet *nova = AllSpaningTree(xl, yl, xll, yll);
-		cout<<nova->getObj(0)<<" "<<nova->getObj(1)<<endl;
+	FirstPhase();
+	stable_sort (suportadas.begin(), suportadas.end(),auxEdgeStructCompSolutions);
+	phase2KB();
+	cout<<"TOTAL DE SOLUCOES = "<<suportadas.size()+noSuportadas.size()<<endl;
+	cout<<"SUPORTADAS: "<<endl;
+	for (int i=0; i<suportadas.size(); i++){
+		cout<<suportadas[i]->getObj(0)<<" "<<suportadas[i]->getObj(1)<<endl;
 	}
+	cout<<"\nNAO SUPORTADAS: "<<endl;
+	for (int i=0; i<noSuportadas.size(); i++){
+		cout<<noSuportadas[i]->getObj(0)<<" "<<noSuportadas[i]->getObj(1)<<endl;
+	}
+
+
+	// stable_sort (arestas, arestas + NUMARESTAS,auxEdgeStructLexCompObj1);
+
+	// SolucaoEdgeSet *pii = new SolucaoEdgeSet(NUMEROVERTICES-1);
+	// lkr.executar (*pii, arestas); //esta versao do kruskal considera que as arestas ja estao ordenadas
+	
+	// pii->objetivoGeral = pii->getObj(0);
+	// Heap.push_back(pii);
+	// std::vector<SolucaoEdgeSet * > sollsk;
+	// for (int i=0; i<MAX_K_BEST && Heap.size()>0; i++){ // OBS.: NO MAXIMO 3000000 de 
+	// 	float xl  = 0;
+	// 	float yl  = 1;
+	// 	float xll = 0;
+	// 	float yll = 0;
+	// 	SolucaoEdgeSet *nova = AllSpaningTree(xl, yl, xll, yll);
+	// 	cout<<i<<" --> "<<nova->getObj(0)<<" "<<nova->getObj(1)<<" size = "<<Heap.size()<<endl;
+	// 	if (i>100 && i<5000){
+	// 		SolucaoEdgeSet * nn = new SolucaoEdgeSet(NUMEROVERTICES-1);
+	// 		*nn = *nova;
+	// 		sollsk.push_back(nn);
+	// 	}
+	// 	delete nova;
+	// }
+	// int i = 0;
+	// for (std::vector<SolucaoEdgeSet* >::iterator it = Heap.begin() ; it != Heap.end(); ++it){
+	// 	cout<<"Delete = "<<i<<endl;
+	// 	delete *it;
+	// 	i++;
+	// }
+	// Heap.clear();
+	// cout<<"SOLUCOES"<<endl;
+	// for (int i=0; i<sollsk.size(); i++){
+	// 	cout<<i<<" --> "<<sollsk[i]->getObj(0)<<" "<<sollsk[i]->getObj(1)<<endl;	
+	// }
+
+	// while (true);
 	// for (int i=0; i<suportadas.size(); i++){
 	// 	// suportadas[i]->printSolucao(stdout);
 	// 	cout<<suportadas[i]->getObj(0)<<" "<<suportadas[i]->getObj(1)<<endl;
